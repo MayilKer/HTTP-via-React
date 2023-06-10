18264ccb4621cc6db0206ddb20425c3949b893aa
MayilZion <kerimov0410@gmail.com>
2023-06-10 21:38:16 +0400

    Second

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  let content = <p>Found no movies</p>
+
+  if (isLoading) {
+    content = <p>Loading....</p>
+  }
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+
+  if (error) {
+    content = error;
+  }
+
+  async function fetchMoviesHandler() {
+    setError(null);
+    setIsLoading(true);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+      };
+    });
+    setMovies(transformedMovies);
+    }
+    catch(error){
+      setError(error.message)
+    }
+    setIsLoading(false);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {content}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
